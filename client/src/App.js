7e858648e98cb8c094d72d00af490481a158a87a
phuyen329 <phuyen329@gmail.com>
2022-05-24 12:48:33 +0700

    User layout

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 //style
 import './App.css';
-import "./assets/styles/bootstrap.min.css"
-import "./assets/styles/fontawesome.min.css"
-import "./assets/styles/slick-theme.min.css"
-import "./assets/styles/slick.min.css"
+import "./user/assets/styles/bootstrap.min.css"
+import "./user/assets/styles/fontawesome.min.css"
+import "./user/assets/styles/slick-theme.min.css"
+import "./user/assets/styles/slick.min.css"
 // import "./assets/styles/templatemo.min.css"
 
 //libraries
@@ -12,17 +12,15 @@ import {BrowserRouter , Routes, Route} from "react-router-dom"
 //admi
 import AdminLanding  from "./admin/Landing"
 //user
-import Landing from './pages/Landing';
-import Home from './pages/Home';
-import Footer from './layouts/Footer';
-import Auth from './pages/Auth';
+import UserLanding from './user/Landing'
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/manage/*' element={<AdminLanding />}/>
-        <Route path='/' element={<Home />} />
-        <Route path="/auth" element={<Auth/>} />
+      <Route path='/manage/*' element={<AdminLanding />}/>
+        <Route path='/*' element={<UserLanding />} />
+        {/* <Route path="/auth" element={<Auth/>} /> */}
         <Route path='*' element= {<div>Nothing here</div>} />
       </Routes>
     </BrowserRouter>
---
+++ client/src/user/pages/Home.js
@@ -0,0 +1,13 @@
+//components
+import Topbar from "../layouts/Topnav"
+import Banner from "../layouts/Banner"
+import Header from "../layouts/Header"
+const Home  = () => {
+    return <>
+        <Topbar/>
+        <Header />
+        <Banner />
+    </>
+}
+
+export default Home
