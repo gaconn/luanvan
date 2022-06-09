cfabc87dbe21b9ffa9533e6c868bed1cd2f758e7
PhuYen329 <phuyen329@gmail.com>
2022-06-09 17:28:08 +0700

    update Client tailwindCSS

--- client/src/user/components/Register/index.js
+++ client/src/user/components/Register/index.js
@@ -1,154 +1,61 @@
 const RegisterComponent = () => {
-    return ( 
+    return (
 
         <>
-        {/* component */}
-        <link
-            rel="stylesheet"
-            href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css"
-        />
-        <link
-            rel="stylesheet"
-            href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css"
-        />
-        <section className=" bg-blueGray-50">
-            <div className="w-full lg:w-6/12 px-4 mx-auto pt-6">
-                <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
-                    <div className="rounded-t mb-0 px-6 py-6">
-                        <div className="text-center mb-3">
-                            <h6 className="text-blueGray-500 text-sm font-bold">
-                                Sign up with
-                            </h6>
-                        </div>
-                        <div className="btn-wrapper text-center">
-                            <button
-                                className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-2 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                                type="button"
-                            >
-                                <img
-                                    alt="..."
-                                    className="w-5 mr-1"
-                                    src="https://demos.creative-tim.com/notus-js/assets/img/github.svg"
-                                />
-                                Github{" "}
-                            </button>
-                            <button
-                                className="bg-white active:bg-blueGray-50 text-blueGray-700 font-normal px-4 py-2 rounded outline-none focus:outline-none mr-1 mb-1 uppercase shadow hover:shadow-md inline-flex items-center font-bold text-xs ease-linear transition-all duration-150"
-                                type="button"
-                            >
-                                <img
-                                    alt="..."
-                                    className="w-5 mr-1"
-                                    src="https://demos.creative-tim.com/notus-js/assets/img/google.svg"
-                                />
-                                Google
-                            </button>
-                        </div>
-                        <hr className="mt-6 border-b-1 border-blueGray-300" />
-                    </div>
-                    <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-                        <div className="text-blueGray-400 text-center mb-3 font-bold">
-                            <small>Or sign up with credentials</small>
-                        </div>
-                        <form>
-                            <div className="relative w-full mb-3">
+
+            <div className="h-screen bg-gradient-to-br from-blue-600 to-indigo-600 flex justify-center items-center w-full">
+                <form>
+                    <div className="bg-white px-10 py-8 rounded-xl w-screen shadow-md max-w-sm">
+                        <div className="space-y-4">
+                            <h1 className="text-center text-2xl font-semibold text-gray-600">
+                                Register
+                            </h1>
+                            <div>
                                 <label
-                                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                                    htmlfor="grid-password"
+                                    htmlFor="email"
+                                    className="block mb-1 text-gray-600 font-semibold"
                                 >
-                                    {" "}
-                                    Name
+                                    Username
                                 </label>
                                 <input
-                                    type="email"
-                                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                    placeholder="Name"
+                                    type="text"
+                                    className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
                                 />
                             </div>
-                            <div className="relative w-full mb-3">
+                            <div>
                                 <label
-                                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                                    htmlfor="grid-password"
+                                    htmlFor="email"
+                                    className="block mb-1 text-gray-600 font-semibold"
                                 >
                                     Email
                                 </label>
                                 <input
-                                    type="email"
-                                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                    placeholder="Email"
+                                    type="text"
+                                    className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
                                 />
                             </div>
-                            <div className="relative w-full mb-3">
+                            <div>
                                 <label
-                                    className="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                                    htmlfor="grid-password"
+                                    htmlFor="email"
+                                    className="block mb-1 text-gray-600 font-semibold"
                                 >
                                     Password
                                 </label>
                                 <input
-                                    type="password"
-                                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                                    placeholder="Password"
+                                    type="text"
+                                    className="bg-indigo-50 px-4 py-2 outline-none rounded-md w-full"
                                 />
                             </div>
-                            <div>
-                                <label className="inline-flex items-center cursor-pointer">
-                                    <input
-                                        id="customCheckLogin"
-                                        type="checkbox"
-                                        className="form-checkbox border-0 rounded text-blueGray-700 ml-1 w-5 h-5 ease-linear transition-all duration-150"
-                                    />
-                                    <span className="ml-2 text-sm font-semibold text-blueGray-600">
-                                        I agree with the
-                                        <a href="javascript:void(0)" className="text-pink-500">
-                                            Privacy Policy
-                                        </a>
-                                    </span>
-                                </label>
-                            </div>
-                            <div className="text-center mt-6">
-                                <button
-                                    className="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                                    type="button"
-                                >
-                                    Create Account
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <footer className="relative  pt-8 pb-6 mt-8">
-                <div className="container mx-auto px-4">
-                    <div className="flex flex-wrap items-center md:justify-between justify-center">
-                        <div className="w-full md:w-6/12 px-4 mx-auto text-center">
-                            <div className="text-sm text-blueGray-500 font-semibold py-1">
-                                Made with{" "}
-                                <a
-                                    href="https://www.creative-tim.com/product/notus-js"
-                                    className="text-blueGray-500 hover:text-gray-800"
-                                    target="_blank"
-                                >
-                                    Notus JS
-                                </a>{" "}
-                                by{" "}
-                                <a
-                                    href="https://www.creative-tim.com"
-                                    className="text-blueGray-500 hover:text-blueGray-800"
-                                    target="_blank"
-                                >
-                                    {" "}
-                                    Creative Tim
-                                </a>
-                                .
-                            </div>
                         </div>
+                        <button className="mt-4 w-full bg-gradient-to-tr from-blue-600 to-indigo-600 text-indigo-100 py-2 rounded-md text-lg tracking-wide">
+                            Register
+                        </button>
                     </div>
-                </div>
-            </footer>
-        </section>
-    </>
-     );
+                </form>
+            </div>
+        </>
+
+    );
 }
- 
+
 export default RegisterComponent;
--- client/src/user/pages/Contact.js
+++ client/src/user/pages/Contact.js
@@ -2,7 +2,7 @@ import Header from "../layouts/Header";
 import Footer from "../layouts/Footer/Footer"
 import NavbarHeader from "../layouts/NavBar"
 import Section from "../layouts/Section"
-import Banner from "../layouts/Banner"
+
 import ContactComponent from '../components/Contact'
 const Contact = () => {
     return (
