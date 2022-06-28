581024ce8846c09fad5618f7fc2d846b564608c3
Quandd <quandd@miczone.asia>
2022-06-28 13:54:37 +0700

    checkout v2

--- api/routes/order.js
+++ api/routes/order.js
@@ -5,5 +5,6 @@ const OrderController = require('../controllers/orderController')
 
 router.get('/get-orders', OrderController.getOrder)
 router.post('/checkout-v1', OrderController.checkoutV1)
+router.post('/checkout-v2', OrderController.checkoutV2)
 
 module.exports = router
