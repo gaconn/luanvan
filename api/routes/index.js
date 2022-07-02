d210aff751dbeaa68c36899ea9c17a080b406f89
Quandd <quandd@miczone.asia>
2022-07-02 10:18:25 +0700

    insert cart, update cart, remove cart

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,7 @@ const supplierRouter = require("./supplier")
 const categoryRouter = require("./category")
 const orderRouter = require('./order')
 const cartRouter = require('./cart')
+const cartItemRouter = require('./cartItem')
 function routes(app) {
     app.use("/product", productRouter)
     app.use("/user", userRouter)
@@ -11,6 +12,7 @@ function routes(app) {
     app.use("/category", categoryRouter)
     app.use("/order", orderRouter)
     app.use("/cart", cartRouter)
+    app.use("/cart-item", cartItemRouter)
 }
 
 module.exports = routes
