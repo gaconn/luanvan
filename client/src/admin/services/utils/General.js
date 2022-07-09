6a573c196bb275e7b74c61b5ea6f9238e009cd0e
Quandd <quandd@miczone.asia>
2022-07-09 16:12:38 +0700

    manage user

--- client/src/admin/services/utils/General.js
+++ client/src/admin/services/utils/General.js
@@ -6,3 +6,35 @@ export const StatusOrder = {
     4: "Đã hoàn thành",
     5: "Đã hủy"
 }
+
+export const toTimeString = (timestamp) => {
+    const date = new Date(timestamp)
+    if(!date) return ''
+    var strDatetime = ""
+
+    if(date.getDate()*1 <10) {
+        strDatetime += `0${date.getDate()}`
+    }else {
+        strDatetime += date.getDate()
+    }
+
+    if(date.getMonth() <10) {
+        strDatetime += ` / 0${date.getMonth()+1}`
+    } else {
+        strDatetime += ' / '+(date.getMonth()+1)
+    }
+
+    strDatetime += ' / ' + date.getFullYear()
+
+    return strDatetime
+}
+
+export const formatDateForInput = (timestamp) => {
+    const date = new Date(timestamp)
+
+    var strDatetime = ""
+    strDatetime += date.getFullYear()
+    strDatetime += '-' + (date.getMonth()+1 >10 ? date.getMonth()+1 : `0${date.getMonth()+1}`)
+    strDatetime += '-' + (date.getDate() > 10 ? date.getDate() : `0${date.getDate()}`)
+    return strDatetime
+}
