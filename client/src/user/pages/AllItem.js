a2c8e47eca2f38227948f54315659048cc58abce
Quandd <quandd@miczone.asia>
2022-07-21 17:54:39 +0700

    update

--- client/src/user/pages/AllItem.js
+++ client/src/user/pages/AllItem.js
@@ -2,6 +2,7 @@
 import { Outlet } from "react-router-dom";
 import Header from "../layouts/Header"
 import NavbarHeader from "../layouts/NavBar"
+import Section from "../layouts/Section";
 
 
 const AllItem = () => {
